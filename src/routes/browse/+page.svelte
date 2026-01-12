98435a4f6a2d0acee42a14ec4c67afcb15276dd4
Developer <user@example.com>
2026-01-12 05:48:07 -0800

    Make browse section cards side by side in mobile view

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -58,7 +58,7 @@
 
 		<!-- Books Grid -->
 		{#if filteredBooks.length > 0}
-			<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+			<div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
 				{#each filteredBooks as book}
 					<BookCard {book} />
 				{/each}
