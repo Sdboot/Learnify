ffb8edc33b5c5905f2199ee220b00bf97cce62bc
Developer <user@example.com>
2026-01-12 05:43:23 -0800

    Make featured books cards side by side in mobile view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,7 +93,7 @@
 				</a>
 			</div>
 
-			<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+			<div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
 				{#each featuredBooks as book}
 					<BookCard {book} />
 				{/each}
